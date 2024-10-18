08cb786030ee0048d04409cac2f10a7a59bfc483
Dylan Lu <dylanelu@gmail.com>
2024-10-17 21:21:06 -0700

    starting recording feature

--- PingPong/Services/CameraFeedService.swift
+++ PingPong/Services/CameraFeedService.swift
@@ -8,9 +8,17 @@
 import Foundation
 import AVFoundation
 
+
 class CameraFeedService: NSObject, ObservableObject {
+    // Livestream
     let captureSession = AVCaptureSession()
     var onFrameCaptured: ((CMSampleBuffer) -> Void)?
+    
+    // Recording game
+    var assetWriter: AVAssetWriter?
+    var videoInput: AVAssetWriterInput?
+    var pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor?
+    var isRecording = false
     
     override init() {
         super.init()
@@ -36,10 +44,69 @@ class CameraFeedService: NSObject, ObservableObject {
             self.captureSession.startRunning()
         }
     }
-}
+    
+    func setupAssetWriter(outputURL: URL, frameSize: CGSize) {
+        do {
+            assetWriter = try AVAssetWriter(outputURL: outputURL, fileType: .mov)
+            
+            let outputSettings: [String: Any] = [
+                AVVideoCodecKey: AVVideoCodecType.h264,
+                AVVideoWidthKey: frameSize.width,
+                AVVideoHeightKey: frameSize.height
+            ]
+            videoInput = AVAssetWriterInput(mediaType: .video, outputSettings: outputSettings)
+            
+            if let assetWriter = assetWriter, let videoInput = videoInput {
+                if assetWriter.canAdd(videoInput) {
+                    assetWriter.add(videoInput)
+                }
+                
+                let sourcePixelBufferAttributes: [String: Any] = [
+                    kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
+                    kCVPixelBufferWidthKey as String: frameSize.width,
+                    kCVPixelBufferHeightKey as String: frameSize.height
+                ]
+                pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(
+                    assetWriterInput: videoInput,
+                    sourcePixelBufferAttributes: sourcePixelBufferAttributes
+                )
+            }
+        } catch {
+            print("Failed to set up AVAssetWriter: \(error)")
+        }
+    }
+    
+    func startRecording(outputURL: URL, frameSize: CGSize) {
+        setupAssetWriter(outputURL: outputURL, frameSize: frameSize)
+        assetWriter?.startWriting()
+        assetWriter?.startSession(atSourceTime: CMTime.zero)
+        isRecording = true
+    }
+    
+    func stopRecording(completion: @escaping () -> Void) {
+        guard isRecording else { return }
+        isRecording = false
+        videoInput?.markAsFinished()
+        assetWriter?.finishWriting {
+            print("Finished writing to file")
+            completion()
+        }
+    }}
 
 extension CameraFeedService: AVCaptureVideoDataOutputSampleBufferDelegate {
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // Send to MediaPipe
         onFrameCaptured?(sampleBuffer)
+        
+        // Record video input
+        guard isRecording, let pixelBufferAdaptor = pixelBufferAdaptor, let videoInput = videoInput, videoInput.isReadyForMoreMediaData else {
+            return
+        }
+        
+        let presentationTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            pixelBufferAdaptor.append(pixelBuffer, withPresentationTime: presentationTime)
+        }
+            
     }
 }
